7b5fecb20bfc8fcc451e7fcab928693553f20784
Arun Ajay <arunajay42795@gmail.com>
2019-04-07 01:02:19 -0400

    added alert function via email

--- src/reddilert.py
+++ src/reddilert.py
@@ -6,16 +6,11 @@ import os
 import config
 from datetime import datetime as dt
 
-#brands = open("names.txt").read().splitlines()
+import smtplib
+import email
+from email.mime.text import MIMEText
 
-'''
-current_dir = os.path.dirname(__file__)
-parent_dir = os.path.split(current_dir)[0]
-file_path = os.path.join(parent_dir,'TestLists/','names.txt')
-brands = open(file_path)
-'''
-
-brands = open(os.path.join(os.path.split(os.path.dirname(__file__))[0],'TestLists/','names.txt')).read().splitlines()
+brands = open(os.path.join(os.path.split(os.path.dirname(__file__))[0],'names.txt')).read().splitlines()
 #TODO: USE config json...
 
 
@@ -40,23 +35,59 @@ def key_word_check(key_list,title,title_storage):
             return True
     return False
 
+def alert(item_list):
+
+
+    fromaddr = config.sender
+    toaddr = config.receiver
+    smtp_user = config.sender
+    smtp_password = config.emailpassword
+    smtp_server = "smtp.gmail.com:587"
+
+    body = ""
+
+    for title,url in item_list:
+        body = body + title + "\n" + url +  "\n\n"
+
+    title = 'My title'
+    msg_content = '<h2>{title} > <font color="green">OK</font></h2>\n'.format(title=title)
+    message = MIMEText(msg_content, 'html')
+
+    message['From'] = fromaddr
+    message['To'] = toaddr
+    message['Subject'] = 'New Deals' + get_time()
+
+    msg_full = body
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(smtp_user, smtp_password)
+    server.sendmail(fromaddr,
+                    [toaddr, toaddr],
+                    msg_full)
+    server.quit()
+    
+
+
 
 while True:
-    daily_list = []
+    old_deals_list = []
     start_time = get_time()
-    print(start_time)
     while True:
+        new_deals_list = []
         for submission in reddit.subreddit('frugalmalefashion').new(limit=10):
             subtitle = submission.title
             suburl = submission.url
-            if(key_word_check(brands,subtitle,daily_list)):
-                    print(subtitle,"\n",suburl,"\n")
-                    daily_list.append(subtitle)
+            if(key_word_check(brands,subtitle,old_deals_list)):
+                    new_deals_list.append((subtitle,suburl))
+                    old_deals_list.append(subtitle)
+        if new_deals_list:
+            print("Emailing New Deals!")
+            alert(new_deals_list)
         time.sleep(5) #scan every minute
         current_time = get_time()
         time_diff = time_subtract(start_time,current_time)
+        print("Time elapsed:",time_diff)
         if( time_diff > 30):
-            print("RESETTING LIST")
             break
-        print(time_diff," seconds have passed")
 
